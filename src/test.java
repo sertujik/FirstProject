239f56f21fbdc41e877deed79b3e0e1e4b8a231a
sertujik <121523469+sertujik@users.noreply.github.com>
2022-12-28 20:04:46 -0800

    I added second line

--- src/test.java
+++ src/test.java
@@ -2,3 +2,13 @@ this is my first line
 this is my second line
 
 I added this line from GitHub.
+
+
+commit and push - when you change something from your local laptop, to send them to github (server),
+        you will use commit and push button.
+
+commit- save my changes to my local repo
+push- sending your committed changes to guthub.class
+if you wanst to get the latest code from server
+git->pull
+pull=>> pull the latest changes from the github.
